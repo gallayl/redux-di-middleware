f05ac3a01678ad870ef1cc1fc9e59b4730f2e4f1
gallayl <gallay.lajos@gmail.com>
2018-07-18 16:38:26 +0200

    initial implementation and tests

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { Action, Dispatch, Middleware } from "redux";
+
+export const defaultContainerName = "default";
+
+export type IInjectableActionCallback<TState, TAction extends Action> = (options: {
+    getState: () => TState,
+    dispatch: Dispatch<TAction>,
+    getInjectable: <TInjectable>(injectableType: { new(...args: any[]): TInjectable }, containerName?: string) => TInjectable,
+}) => any;
+
+export interface InjectableAction<TState, TAction extends Action> extends Action {
+    inject: IInjectableActionCallback<TState, TAction>;
+}
+
+export const isInjectableAction = <TState, TAction extends Action>(action: any): action is InjectableAction<TState, TAction> => {
+    return action && action.inject && typeof action.inject === "function";
+};
+
+export class ReduxDiMiddleware {
+    private readonly containers: Map<string, Map<string, any>> = new Map<string, Map<string, any>>([
+        [defaultContainerName, new Map<string, any>()] as [string, Map<string, any>],
+    ]);
+
+    public setInjectable<T>(value: T, containerName: string = defaultContainerName) {
+        let container!: Map<string, any>;
+        if (!this.containers.has(containerName)) {
+            this.containers.set(containerName, new Map());
+        }
+        container = this.containers.get(containerName) as Map<string, any>;
+        container.set(value.constructor.name, value);
+    }
+
+    public getInjectable<T>(injectableType: { new(...args: any[]): T }, containerName: string = defaultContainerName) {
+        if (this.containers.has(containerName)) {
+            const container = this.containers.get(containerName) as Map<string, any>;
+            if (container.has(injectableType.name)) {
+                return container.get(injectableType.name) as T;
+            }
+            throw Error(`Injectable not found for '${injectableType.name}' in the container '${containerName}'`);
+        }
+        throw Error(`No container found with name '${containerName}'`);
+    }
+
+    public getMiddleware: () => Middleware = () => (api) => (next) => (action: InjectableAction<{}, Action>) => {
+        if (isInjectableAction(action)) {
+            return action.inject({
+                dispatch: api.dispatch,
+                getState: api.getState,
+                getInjectable: this.getInjectable,
+            });
+        } else {
+            return next(action);
+        }
+    }
+}
---
+++ test/index.ts
@@ -0,0 +1,82 @@
+import { expect } from "chai";
+import { Action, applyMiddleware, createStore, Reducer } from "redux";
+import { defaultContainerName, InjectableAction, ReduxDiMiddleware } from "../src/index";
+
+export class ExampleInjectable {
+    constructor(public readonly value: string) { }
+}
+
+export const tests = describe("ReduxDiMiddleware", () => {
+    it("Can be constructed", () => {
+        const m = new ReduxDiMiddleware();
+        expect(m).to.be.instanceof(ReduxDiMiddleware);
+    });
+
+    describe("Injectables", () => {
+        it("Should be added to default container", () => {
+            const m = new ReduxDiMiddleware();
+            const injectable = new ExampleInjectable("asd");
+            m.setInjectable(injectable);
+            expect(injectable).to.be.eq(m.getInjectable(ExampleInjectable));
+        });
+
+        it("Should be added to another container", () => {
+            const m = new ReduxDiMiddleware();
+            const injectable = new ExampleInjectable("asd");
+            m.setInjectable(injectable, "container2");
+            expect(injectable).to.be.eq(m.getInjectable(ExampleInjectable, "container2"));
+
+            // tslint:disable-next-line:no-string-literal
+            expect((m["containers"].get(defaultContainerName) as any).size).to.be.eq(0);
+        });
+
+        it("Should throw an error if no injectable is available in the container", () => {
+            const m = new ReduxDiMiddleware();
+            expect(() => m.getInjectable(ExampleInjectable)).to.throws();
+        });
+    });
+
+    describe("Containers", () => {
+        it("Should define its own default container", () => {
+            const m = new ReduxDiMiddleware();
+            // tslint:disable-next-line:no-string-literal
+            expect(m["containers"].has(defaultContainerName)).to.be.eq(true);
+        });
+
+        it("Should be created lazily on adding a new entry", () => {
+            const m = new ReduxDiMiddleware();
+            m.setInjectable(new ExampleInjectable("alma"), "container2");
+            // tslint:disable-next-line:no-string-literal
+            expect(m["containers"].has("container2")).to.be.eq(true);
+        });
+
+        it("Should throw meaningful error when try to get value from an undefined container", () => {
+            const m = new ReduxDiMiddleware();
+            expect(() => m.getInjectable(ExampleInjectable, "container2")).to.throws();
+        });
+    });
+
+    describe("middleware", () => {
+
+        const mockReducer: Reducer<{ value: string }, Action> = (state = { value: "asd" }, action: Action) => state;
+
+        it("Should be registered into a Redux store", (done: MochaDone) => {
+            const m = new ReduxDiMiddleware();
+            m.setInjectable(new ExampleInjectable("asdddd"));
+            const store = createStore(mockReducer, applyMiddleware(m.getMiddleware()));
+            const action: InjectableAction<any, Action> = ({
+                type: "AAA",
+                inject: (options) => {
+                    expect(options.getState()).to.be.deep.eq({ value: "asd" });
+                    expect(options.getInjectable(ExampleInjectable).value).to.be.eq("asdddd");
+                    done();
+                },
+            });
+
+            store.dispatch({ type: "AAA" });
+
+            store.dispatch(action);
+        });
+    });
+
+});
